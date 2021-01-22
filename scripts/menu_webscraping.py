e6b553ebd74d3cb8ede7bc8ac8470a59f0bce5e8
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-01-21 19:12:05 -0600

    rough start at script to webscrape menu data

--- scripts/menu_webscraping.py
+++ scripts/menu_webscraping.py
@@ -1,1 +1,13 @@
+from bs4 import BeautifulSoup
 import requests
+
+url = "https://drexel.campusdish.com/LocationsAndMenus/UrbanEatery?locationId=9853&storeIds=&mode=Weekly&date=01%2F22%2F2021&periodId=2480"
+page = requests.get(url)
+
+# print(page.content)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find_all('div', {'class':'menu__station'})
+
+for result in results:
+    print(result)
